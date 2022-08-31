d4724e25c0f204c10e8506ca2003056b4927d8e2
cunmo <cunmo_sir@163.com>
2022-08-31 23:55:21 +0800

    fix: 利用Promise 实现接口的延迟返回

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const koa = require('koa')
 const app = new koa()
 
+function deely(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, time);
+  })
+}
+
+// app.use(async (ctx, next) => {
+//   ctx.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+//   // setTimeout(() => {
+//     await next()
+//   // }, 3000)
+// })
+
 app.use(async (ctx, next) => {
-  let json = {"a": '1000'}
   ctx.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-  setTimeout(() => {
-    ctx.body = json
-    // await next()
-  }, 5000)
+  let json = {"a": '1000'}
+  await deely(3000)
+  ctx.body = json
+  await next()
 })
 
 app.listen('9000', () => {
